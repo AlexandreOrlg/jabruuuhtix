9c847632b3293fa3969fdf386ad8e7abb198fed7
AlexandreOrlg <63079782+AlexandreOrlg@users.noreply.github.com>
2026-01-10 14:00:28 +0100

    feat(ui): add repo footer link
    - Surface the open-source repo from the footer\n- Add favicon assets and manifest for the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -91,7 +91,15 @@ function App() {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 60 60" height={24} className="inline mx-3"><path fill="#fff" d="M12 18h6v6h-6v-6ZM18 6h6v6h-6V6Zm-6 0h6v6h-6V6Zm12 6h6v6h-6v-6Zm6 0h6v6h-6v-6Zm6-6h6v6h-6V6Zm6 0h6v6h-6V6Zm6 0h6v6h-6V6Zm6 6h6v6h-6v-6Zm0 6h6v6h-6v-6ZM6 6h6v6H6V6Zm-6 6h6v6H0v-6Zm0 6h6v6H0v-6Zm0 6h6v6H0v-6Zm54 0h6v6h-6v-6ZM6 30h6v6H6v-6Zm6 6h6v6h-6v-6Zm6 6h6v6h-6v-6Zm6 6h6v6h-6v-6Zm6 0h6v6h-6v-6Zm6-6h6v6h-6v-6Zm6-6h6v6h-6v-6Zm6-6h6v6h-6v-6Z" /><path fill="#9D0000" d="M6 12h6v6H6v-6Z" /><path fill="red" d="M12 12h6v6h-6v-6Zm6 0h6v6h-6v-6Z" /><path fill="#9D0000" d="M6 18h6v6H6v-6Z" /><path fill="red" d="M12 18h6v6h-6v-6Zm6 0h6v6h-6v-6Z" /><path fill="#9D0000" d="M6 24h6v6H6v-6Z" /><path fill="red" d="M12 24h6v6h-6v-6Zm6 0h6v6h-6v-6Z" /><path fill="#9D0000" d="M12 30h6v6h-6v-6Z" /><path fill="red" d="M18 30h6v6h-6v-6Z" /><path fill="#9D0000" d="M18 36h6v6h-6v-6Z" /><path fill="red" d="M24 36h6v6h-6v-6Z" /><path fill="#9D0000" d="M24 42h6v6h-6v-6Z" /><path fill="red" d="M30 42h6v6h-6v-6Zm0-6h6v6h-6v-6Zm-6-6h6v6h-6v-6Zm6 0h6v6h-6v-6Zm0-6h6v6h-6v-6Zm-6 0h6v6h-6v-6Zm0-6h6v6h-6v-6Zm6 0h6v6h-6v-6Zm6 6h6v6h-6v-6Zm6 6h6v6h-6v-6Zm-6 0h6v6h-6v-6Zm0 6h6v6h-6v-6Zm6-12h6v6h-6v-6Z" /><path fill="#FF5757" d="M48 24h6v6h-6v-6Z" /><path fill="red" d="M36 18h6v6h-6v-6Zm6 0h6v6h-6v-6Z" /><path fill="#FF5757" d="M48 18h6v6h-6v-6Zm0-6h6v6h-6v-6Zm-6 0h6v6h-6v-6Zm-6 0h6v6h-6v-6Z" /></svg>
         sous le
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 78 78" height={24} className="inline mx-3"><g clip-path=" url(#a)"><path fill="#FFC41D" d="M46 20.5c-4.833.667-14.8 1.6-16 0V24l-7.5 6.5L22 46l10 11 14-.5 9-9.5V31l-4.5-3.5-4.5-7Z" /><path fill="#fff" d="M24 30v-6h6v6h-6ZM30 24v-6h6v6h-6ZM36 24v-6h6v6h-6ZM42 24v-6h6v6h-6ZM48 30v-6h6v6h-6ZM60 18v-6h6v6h-6ZM18 60v6h-6v-6h6ZM18 18v-6h6v6h-6ZM60 60v6h-6v-6h6ZM66 12V6h6v6h-6ZM12 66v6H6v-6h6ZM12 12V6h6v6h-6ZM66 66v6h-6v-6h6ZM36 12V6h6v6h-6ZM42 66v6h-6v-6h6ZM66 36h6v6h-6v-6ZM12 42H6v-6h6v6ZM0 36h6v6H0v-6ZM78 42h-6v-6h6v6ZM36 6V0h6v6h-6ZM42 72v6h-6v-6h6ZM54 36v-6h6v6h-6ZM54 42v-6h6v6h-6ZM54 48v-6h6v6h-6ZM48 54v-6h6v6h-6ZM42 60v-6h6v6h-6ZM36 60v-6h6v6h-6ZM30 60v-6h6v6h-6ZM24 54v-6h6v6h-6ZM18 48v-6h6v6h-6ZM18 42v-6h6v6h-6ZM18 36v-6h6v6h-6Z" /></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h78v78H0z" /></clipPath></defs></svg>
-        de Marseille
+        de Marseille · Open source :{" "}
+        <a
+          className="underline underline-offset-2 hover:text-white"
+          href="https://github.com/AlexandreOrlg/jabruuuhtix"
+          target="_blank"
+          rel="noreferrer"
+        >
+          github.com/AlexandreOrlg/jabruuuhtix
+        </a>
       </footer >
 
 
